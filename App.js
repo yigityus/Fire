37bc9015a3c8733aca5650436de0ac1401a24ba1
Leaf <leaf@garden.local>
2022-05-29 14:48:14 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,136 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useEffect, useState} from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Alert} from 'react-native';
+import messaging, {firebase} from '@react-native-firebase/messaging';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import database from '@react-native-firebase/database';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        selectable={true}
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const [token, setToken] = useState();
+  const [foo, setFoo] = useState();
+
+  /*
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
+  }, []);
+  */
+  const defaultAppMessaging = firebase.messaging();
+  defaultAppMessaging.getToken().then(t => {
+    setToken(t);
+    console.log(t);
+  });
+  const isDarkMode = useColorScheme() === 'dark';
+  /*
+  database()
+    .ref('/nodes/0x00001')
+    .once('value')
+    .then(snapshot => {
+      console.log('User data: ', snapshot.val());
+      setFoo(snapshot.val());
+    });
+    */
+  database()
+    .ref('/nodes/0x00001')
+    .on('value', snapshot => {
+      console.log('User data: ', snapshot.val());
+      // setFoo(snapshot.val());
+    });
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Registration Token">{token}</Section>
+          <Section title="Realtime Database">
+            {JSON.stringify(foo, 2, 2)}
+          </Section>
+          <Section title="Hello">World!</Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
